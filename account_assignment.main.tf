cc74fdd90655972f86eff687c46ae864d7b6dd9d
Mariusz Słowiński <mario.slowinski@gmail.com>
2024-06-09 19:03:08 +0200

    "permission_set" "var.account_assignments" argument can be ARN or name of resource in this module.

--- account_assignment.main.tf
+++ account_assignment.main.tf
@@ -4,13 +4,13 @@ resource "aws_ssoadmin_account_assignment" "principal" {
     join(":", [
       account_assignment.principal,
       account_assignment.principal_type,
-      account_assignment.permission_set_arn,
+      account_assignment.permission_set,
     ]) => account_assignment
     if account_assignment.principal != null
   }
 
   instance_arn       = coalesce(each.value.instance_arn, local.instances[0].arn)
-  permission_set_arn = coalesce(each.value.permission_set_arn, local.permission_sets[0].arn)
+  permission_set_arn = startswith(each.value.permission_set, "arn:aws:sso:") ? each.value.permission_set : local.permission_sets[each.value.permission_set].arn
   principal_id       = each.value.principal_id
   principal_type     = each.value.principal_type
   target_id          = coalesce(each.value.target_id, data.aws_caller_identity.current.account_id)
