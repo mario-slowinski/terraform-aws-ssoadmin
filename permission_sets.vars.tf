121e37ac9f7aa30e301a441da42bde47610c3c81
Mariusz Słowiński <mario.slowinski@gmail.com>
2024-06-09 20:43:11 +0200

    Add "managed_policy" argument to "var.permission_sets" to have a static name in for_each.

--- permission_sets.vars.tf
+++ permission_sets.vars.tf
@@ -1,11 +1,13 @@
 variable "permission_sets" {
   type = list(object({
-    description      = optional(string)      # The description of the Permission Set.
-    instance_arn     = optional(string)      # The Amazon Resource Name (ARN) of the SSO Instance. Use the one from module if not given.
-    name             = string                # (Required, Forces new resource) The Amazon Resource Name (ARN) of the SSO Instance under which the operation will be executed.
-    relay_state      = optional(string)      # The relay state URL used to redirect users within the application during the federation authentication process.
-    session_duration = optional(string)      # The length of time that the application user sessions are valid in the ISO-8601 standard. Default: PT1H.
-    tags             = optional(map(string)) # Key-value map of resource tags.
+    description        = optional(string)      # The description of the Permission Set.
+    instance_arn       = optional(string)      # The Amazon Resource Name (ARN) of the SSO Instance. Use the one from module if not given.
+    name               = string                # (Required, Forces new resource) The Amazon Resource Name (ARN) of the SSO Instance under which the operation will be executed.
+    relay_state        = optional(string)      # The relay state URL used to redirect users within the application during the federation authentication process.
+    session_duration   = optional(string)      # The length of time that the application user sessions are valid in the ISO-8601 standard. Default: PT1H.
+    tags               = optional(map(string)) # Key-value map of resource tags.
+    managed_policy     = optional(string)      # The IAM managed policy name to be attached to the Permission Set.
+    managed_policy_arn = optional(string)      # The IAM managed policy Amazon Resource Name (ARN) to be attached to the Permission Set.
   }))
   description = "List of Single Sign-On (SSO) Permission Sets."
   default     = [{ name = null, }]
